20ca8b71e13d15e302599836f33382342a27c9e9
Andereyan Muhammat <andereyan@gmail.com>
2026-06-24 12:22:52 +0800

    feat: create frontend layout component with support for Filament styles, dark mode toggle, and global components

--- resources/views/components/layouts/frontend.blade.php
+++ resources/views/components/layouts/frontend.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
     <title>{{ $title ?? 'Welcome - Receiving PKT' }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
