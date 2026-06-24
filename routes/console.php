0182c22a23db2259708455e8b42daccc65101086
Andereyan Muhammat <andereyan@gmail.com>
2026-06-24 18:54:36 +0800

    feat: add scheduled queue worker command to console routes

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Menjalankan antrean (queue) setiap menit tanpa bentrok
+// Ini sangat cocok untuk shared hosting (Hostinger) yang tidak mendukung Supervisor
+Schedule::command('queue:work --stop-when-empty')
+    ->everyMinute()
+    ->withoutOverlapping();
